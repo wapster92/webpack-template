ced447e40dc934d7a43bc48528ece497be6bf23d
wapster92 <wapster92@gmail.com>
2019-03-28 14:51:49 +0600

    small fix

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -151,7 +151,7 @@ module.exports = {
       apiOptions: {
           cssImageRef: `../img/sprite.png`
       },
-      retina: '@2x'
+      //retina: '@2x'
     }),
     new SVGSpritemapPlugin(`${PATHS.src.assets}/sprite/**/*.svg`, {
       output: {filename: '/assets/img/sprite.svg'},
